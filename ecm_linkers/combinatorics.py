732acad88cbad62ed3164750eac3914d7a1999df
i.goltsov <ilya_goltsov@list.ru>
2021-04-16 18:12:31 +0300

    Добавлена генерация комплектов контейнеров с суммарной вместимостью, равной числу элементов.

--- ecm_linkers/combinatorics.py
+++ ecm_linkers/combinatorics.py
@@ -1,4 +1,5 @@
 import numpy as np
+import itertools
 
 
 # Функционал, реализующий комбинаторные задачи
@@ -7,8 +8,29 @@ import numpy as np
 # n_elems - количество элементов
 # capacities - list с возможными емкостями контейнеров
 def get_containers_by_elems(n_elems, capacities):
-    # TODO: Ilya
+    capacities.sort()
+    res_array = []
 
-    containers = [2, 5, 6]
+    for m in range(1, n_elems):
+        current_min_sum = capacities[0] * m
+        current_max_sum = capacities[-1] * m
+        if current_min_sum > n_elems or current_max_sum < n_elems:
+            continue
+
+        res_array.append([p for p in itertools.combinations_with_replacement(capacities, r=m)])
+
+    containers = []
+    for res_series in res_array:
+        for res in res_series:
+            if sum(res) == n_elems:
+                containers.append(res)
+
     # На выход - list с перечислением контейнеров, где значение - емкость контейнера
     return containers
+
+
+# if __name__ == '__main__':
+#     containers_list_outer = [2, 4, 6, 7]
+#     n = 20
+#
+#     print(get_containers_by_elems(n, containers_list_outer))
